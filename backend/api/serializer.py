ce07cd538d0c60745cd658c38d86d3a09934a158
Soham Patil <patilsoham390@gmail.com>
2023-09-28 15:42:46 +0530

    Added  some api logic

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -1,6 +1,8 @@
 from.models import *
 from rest_framework import serializers
 
+
+# station serializer logic
 class StationSerializer(serializers.Serializer):
     stationid = serializers.CharField(max_length=100)
     stationname = serializers.CharField(max_length=100)
@@ -14,10 +16,28 @@ class StationSerializer(serializers.Serializer):
     def create(self, validated_data):
         return Station.objects.create(**validated_data)
 
-    # def update(self, station, validated_data):
-    #     newStation = Station(**validated_data)
-    #     newStation.stationid = station.stationid
-    #     newStation.save()
-    #     return newStation
+    def update(self, station, validated_data):
+        newStation = Station(**validated_data)
+        newStation.stationid = station.stationid
+        newStation.save()
+        return newStation
+    
+# Users serializers logic
+class UsersSerializer(serializers.Serializer):
+    user_id = serializers.CharField(max_length=100)
+    user_name = serializers.CharField(max_length=100)
+    phone_no = serializers.IntegerField()
+    email = serializers.EmailField()
+    city = serializers.CharField(max_length=100)
+
+    
+    def create(self, validated_data):
+        return Users.objects.create(**validated_data)
+
+    def update(self, users, validated_data):
+        newUsers = Users(**validated_data)
+        newUsers.stationid = users.stationid
+        newUsers.save()
+        return newUsers
     
 
